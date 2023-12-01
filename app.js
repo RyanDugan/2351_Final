1f105b15a0fd84035467635d3a8e3c14a6e06754
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-11-30 18:03:14 -0600

    added js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+let photoIndex = 1;
+showPhotos(photoIndex);
+
+function plusPhotos(n) {
+  showPhotos((photoIndex += n));
+}
+
+function currentPhoto(n) {
+  showPhotos((photoIndex = n));
+}
+
+function showPhotos(n) {
+  let i;
+  let photos = document.getElementsByClassName("photo");
+  if (n > photos.length) {
+    photoIndex = 1;
+  }
+  if (n < 1) {
+    photoIndex = photos.length;
+  }
+  for (i = 0; i < photos.length; i++) {
+    photos[i].style.display = "none";
+  }
+  photos[photoIndex - 1].style.display = "block";
+}
